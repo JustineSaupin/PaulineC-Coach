9e2a2d1193c4c521e166714955b0b480b0d0178f
Justine Saupin <sau.justine@gmail.com>
2021-04-26 12:46:16 +0200

    burger menu 1st step

--- script.js
+++ script.js
@@ -15,3 +15,16 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+
+// BURGER MENU :
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
