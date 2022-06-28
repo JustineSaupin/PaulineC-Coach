b594bfd4b7856a0fc568f60a013cb12ac5da805d
Justine Saupin <sau.justine@gmail.com>
2022-06-28 15:49:27 +0200

    add calendly

--- script.js
+++ script.js
@@ -30,3 +30,17 @@ function displayMenu() {
   }
 }
 
+
+// Slider Testimonials
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+      items:3,
+      autoplay:true,
+      margin:30,
+      loop:true,
+      dots:true
+      // nav:true,
+//      navText:["<i class='fas fa-long-arrow-alt-left'></i>","<i class='fas fa-long-arrow-alt-right'></i>" ]
+  });
+});
